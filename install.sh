c1db7c0188f67d3adb0915b1a65ad580ceca1f61
davidcraig <d.craig@redu.co.uk>
2018-12-20 16:54:45 +0000

    Add Homebrew installation

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -e
 
+printf "Copying vimrc \n"
 if [ ! -f "$HOME/.vimrc" ]; then
   cp ./.vimrc $HOME/.vimrc
 else
-  printf "vimrc already exists, skipping"
+  printf "vimrc already exists, skipping \n"
 fi
+
+printf "Installing Homebrew \n"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
